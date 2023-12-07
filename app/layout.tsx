056fd62eb6e241e8daa7c2a4adaa2302fa9fac7f
emily020kim <emily020kim@gmail.com>
2023-12-06 20:24:19 -0800

    finishing edits

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,14 @@ import { Urbanist } from 'next/font/google'
 import './globals.css'
 import Footer from '@/components/footer'
 import Navbar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
 
 const font = Urbanist({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Store',
-  description: 'Store',
+  title: 'KREW',
+  description: 'Find the best sneakers here!',
 }
 
 export default function RootLayout({
@@ -19,10 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
       </body>
     </html>
-  )
+  );
 }
